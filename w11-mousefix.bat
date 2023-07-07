78af3a4348c229062af856330a353092d3913205
mitchhvl <73445681+mitchhvl@users.noreply.github.com>
2023-07-07 10:17:55 +0200

    w11-mousefix.bat

--- w11-mousefix.bat
+++ w11-mousefix.bat
@@ -3,17 +3,19 @@
 echo Wijzigingen in het register worden aangebracht. Zorg ervoor dat u dit script als beheerder uitvoert.
 echo.
 
+set "RegKey=HKCU\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
+
 echo 1. Navigeer naar het pad HKEY_CURRENT_USER\SOFTWARE\CLASSES\CLSID
-regedit /s "HKCU\SOFTWARE\CLASSES\CLSID.reg"
+reg import "HKCU\SOFTWARE\CLASSES\CLSID.reg"
 
 echo 2. Een nieuwe sleutel {86ca1aa0-34aa-4e8b-a509-50c905bae2a2} wordt gemaakt.
-reg add "HKCU\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+reg add "%RegKey%" /f >nul
 
 echo 3. Een nieuwe sub-sleutel InprocServer32 wordt gemaakt.
-reg add "HKCU\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f
+reg add "%RegKey%\InprocServer32" /f >nul
 
 echo 4. De waarde van de sub-sleutel InprocServer32 wordt leeggemaakt.
-reg add "HKCU\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /d "" /f
+reg add "%RegKey%\InprocServer32" /ve /d "" /f >nul
 
 echo De wijzigingen zijn aangebracht. Je moet opnieuw aanmelden om de wijzigingen door te voeren.
 echo.
