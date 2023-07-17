61001b18645ea5f2411afa476eab910923cc6fce
mitchhvl <73445681+mitchhvl@users.noreply.github.com>
2023-07-17 15:22:12 +0200

    opschonen-2.bat

--- opschonen-2.bat
+++ opschonen-2.bat
@@ -20,14 +20,33 @@ reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer
 reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\WordWheelQuery" /f >nul
 echo Stap 4 voltooid.
 
-REM Stap 5: File Explorer herstarten.
-taskkill /f /im explorer.exe
-start explorer.exe
+REM Stap 5: Browsercaches verwijderen.
+echo Bezig met het verwijderen van browsercaches...
+
+REM Chrome-cache verwijderen
+start /wait "" "%LocalAppData%\Google\Chrome\Application\chrome.exe" --user-data-dir="%LocalAppData%\Google\Chrome" --disk-cache-size=1
+rmdir /q /s "%LocalAppData%\Google\Chrome\User Data\Default\Cache"
+
+REM Firefox-cache verwijderen
+start /wait "" "%ProgramFiles%\Mozilla Firefox\firefox.exe" -P "default" -safe-mode
+rmdir /q /s "%AppData%\Mozilla\Firefox\Profiles\*.default\cache"
+
+REM Edge-cache verwijderen
+start /wait "" "%LocalAppData%\Microsoft\Edge\Application\msedge.exe" --user-data-dir="%LocalAppData%\Microsoft\Edge" --disk-cache-size=1
+rmdir /q /s "%LocalAppData%\Microsoft\Edge\User Data\Default\Cache"
+
+REM Internet Explorer-cache verwijderen
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
 echo Stap 5 voltooid.
 
-REM Stap 6: Windows Update-service starten.
-net start wuauserv
+REM Stap 6: File Explorer herstarten.
+taskkill /f /im explorer.exe
+start explorer.exe
 echo Stap 6 voltooid.
+
+REM Stap 7: Windows Update-service starten.
+net start wuauserv
+echo Stap 7 voltooid.
 
 echo.
 echo De computer is opgeschoond.
@@ -40,8 +59,11 @@ for /f "usebackq tokens=2 delims=," %%a in (`dir /s "%temp%" ^| findstr "File(s)
 for /f "usebackq tokens=2 delims=," %%b in (`dir /s "%systemroot%\Temp\" ^| findstr "File(s)"`) do set "system_temp_size=%%b"
 for /f "usebackq tokens=2 delims=," %%c in (`dir /s "%systemroot%\Prefetch\" ^| findstr "File(s)"`) do set "prefetch_size=%%c"
 for /f "usebackq tokens=2 delims=," %%d in (`dir /s "%systemroot%\SoftwareDistribution\Download" ^| findstr "File(s)"`) do set "update_cache_size=%%d"
+for /f "usebackq tokens=2 delims=," %%e in (`dir /s "%LocalAppData%\Google\Chrome\User Data\Default\Cache" ^| findstr "File(s)"`) do set "chrome_cache_size=%%e"
+for /f "usebackq tokens=2 delims=," %%f in (`dir /s "%AppData%\Mozilla\Firefox\Profiles\*.default\cache" ^| findstr "File(s)"`) do set "firefox_cache_size=%%f"
+for /f "usebackq tokens=2 delims=," %%g in (`dir /s "%LocalAppData%\Microsoft\Edge\User Data\Default\Cache" ^| findstr "File(s)"`) do set "edge_cache_size=%%g"
 
-set /a "total_size=temp_size + system_temp_size + prefetch_size + update_cache_size"
+set /a "total_size=temp_size + system_temp_size + prefetch_size + update_cache_size + chrome_cache_size + firefox_cache_size + edge_cache_size"
 set /a "total_gb=total_size / (1024*1024*1024)"
 set /a "total_mb=(total_size / (1024*1024)) %% 1024"
 
